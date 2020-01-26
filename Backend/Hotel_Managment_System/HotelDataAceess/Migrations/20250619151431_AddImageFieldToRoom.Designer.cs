cd21d9a40fd6e7ea158f482b95de76b825802e08
psmart28 <powersmart28@gmail.com>
2020-01-26 08:07:23 -0400

    reorganize helper logic

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250619151431_AddImageFieldToRoom.Designer.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250619151431_AddImageFieldToRoom.Designer.cs
@@ -198,6 +198,7 @@ namespace HotelDataAceess.Migrations
                         .HasMaxLength(100)
                         .HasColumnType("nvarchar(100)");
 
+                    b.Property<byte>("Gender")
                         .HasColumnType("tinyint")
                         .HasComment("0 => Male \r\n1 => Female");
 
