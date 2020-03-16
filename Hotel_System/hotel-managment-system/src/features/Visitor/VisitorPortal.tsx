b2d870316c343ebc112885f49bf15104c848f34d
psmart28 <powersmart28@gmail.com>
2020-03-15 22:36:06 -0400

    simplify implementation details

--- Hotel_System/hotel-managment-system/src/features/Visitor/VisitorPortal.tsx
+++ Hotel_System/hotel-managment-system/src/features/Visitor/VisitorPortal.tsx
@@ -31,6 +31,7 @@ export default function VisitorPortal({
       overflow-hidden
       rounded-2xl
       bg-white dark:bg-gray-800
+      shadow-2xl
       border border-gray-200 dark:border-gray-700
     "
         onClick={(e) => e.stopPropagation()}
