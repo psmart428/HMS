d6258b764b904c2a7a83dc496d6d3bf3eabc99a9
psmart28 <powersmart28@gmail.com>
2020-03-17 12:12:09 -0400

    refine service logic

--- Backend/Hotel_Managment_System/Hotel_Server_Side/Controllers/UserController.cs
+++ Backend/Hotel_Managment_System/Hotel_Server_Side/Controllers/UserController.cs
@@ -297,6 +297,7 @@ namespace Hotel_Server_Side.Controllers
             }
         }
 
+        [Authorize(Roles = "Admin,Employee")]
         [HttpGet("CountUsers/{column}/{value}/{Operations}", Name = "CountUsers")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
