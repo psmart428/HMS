d161cfa51d11ff4b25d334569904caab132a22c3
psmart28 <powersmart28@gmail.com>
2020-10-22 16:12:39 -0400

    adjust helper behavior

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250324221501_Initi.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250324221501_Initi.cs
@@ -104,6 +104,7 @@ namespace HotelDataAceess.Migrations
                 },
                 constraints: table =>
                 {
+                    table.PrimaryKey("PK__Users__1788CCACB0319B20", x => x.UserID);
                     table.ForeignKey(
                         name: "FK__Users__PersonID__4CA06362",
                         column: x => x.PersonID,
