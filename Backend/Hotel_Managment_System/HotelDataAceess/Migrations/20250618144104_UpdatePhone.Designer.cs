aaf5f2aafee7c09d199a577727aa30468f55cd19
psmart28 <powersmart28@gmail.com>
2021-01-20 10:31:56 -0400

    refine service logic

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250618144104_UpdatePhone.Designer.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250618144104_UpdatePhone.Designer.cs
@@ -300,6 +300,7 @@ namespace HotelDataAceess.Migrations
 
             modelBuilder.Entity("HotelDataAceess.Entiteis.Views.BookingView", b =>
                 {
+                    b.Property<int>("BookingId")
                         .HasColumnType("int");
 
                     b.Property<DateTime>("CheckInDate")
