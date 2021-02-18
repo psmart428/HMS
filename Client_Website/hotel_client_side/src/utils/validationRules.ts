2779951574c27f3b1cdefb01451a035e9fa603c7
psmart28 <powersmart28@gmail.com>
2021-02-18 11:12:11 -0400

    adjust request handling

--- Client_Website/hotel_client_side/src/utils/validationRules.ts
+++ Client_Website/hotel_client_side/src/utils/validationRules.ts
@@ -28,7 +28,6 @@ export function validatePasswordRule(
 }
 
 export function validateConfirmPasswordRule(
-  getValues: () => FieldValues
 ): RegisterOptions<RegisterValues, "confirmPassword"> {
   return {
     required: "Confirm Password is required",
