0bbfd4cd0cba31a19364599601c0a05ecd8c7a5e
psmart28 <powersmart28@gmail.com>
2021-04-19 11:36:26 -0400

    fix issues in processing steps

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250326205636_UpdatePeopleTable.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250326205636_UpdatePeopleTable.cs
@@ -78,6 +78,7 @@ namespace HotelDataAceess.Migrations
                 fixedLength: true,
                 maxLength: 1,
                 nullable: false,
+                oldClrType: typeof(byte),
                 oldType: "tinyint");
 
             migrationBuilder.AlterColumn<string>(
