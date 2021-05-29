025f00b558f97d25e60a3bd9e75acf93385008cb
psmart28 <powersmart28@gmail.com>
2021-05-29 08:38:28 -0400

    update runtime handling

--- Hotel_System/hotel-managment-system/src/features/User/UserHooks/useSearchUser.ts
+++ Hotel_System/hotel-managment-system/src/features/User/UserHooks/useSearchUser.ts
@@ -14,6 +14,7 @@ export function useSearchUser() {
   const operation: string =
     column === "userId" ? Operations.EQUAL : Operations.STARTS_WITH;
 
+  const shouldFetchUsers = value !== "";
 
   const pageNumber = Number(searchParams.get("page")) || 1;
   const { isLoading, data, error } = useQuery({
