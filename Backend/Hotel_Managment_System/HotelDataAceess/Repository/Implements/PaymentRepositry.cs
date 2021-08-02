fdeb6f546c342b4b25fc3621853fc1fa0739a24a
psmart28 <powersmart28@gmail.com>
2021-08-02 15:26:22 -0400

    refine code path

--- Backend/Hotel_Managment_System/HotelDataAceess/Repository/Implements/PaymentRepositry.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Repository/Implements/PaymentRepositry.cs
@@ -20,7 +20,6 @@ namespace HotelDataAceess.Repository.Implements
 
             return await query
                             .OrderByDescending(p => p.PaymentId)
-                            .Skip((pageNumber - 1) * pageSize)
                             .Take(pageSize)
                             .AsNoTracking()
                             .ToListAsync();
