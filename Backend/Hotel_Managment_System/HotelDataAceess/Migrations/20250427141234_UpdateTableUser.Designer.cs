57e4ad0edf7568793ce9842b3ce222dfc5f4c2e5
psmart28 <powersmart28@gmail.com>
2021-10-04 11:24:56 -0400

    simplify control flow

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250427141234_UpdateTableUser.Designer.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250427141234_UpdateTableUser.Designer.cs
@@ -19,7 +19,6 @@ namespace HotelDataAceess.Migrations
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder
                 .HasAnnotation("ProductVersion", "8.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
