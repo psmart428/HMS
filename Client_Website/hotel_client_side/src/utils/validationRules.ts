1b676d814512601f8c04f78ea8f7b8d670b5f823
psmart28 <powersmart28@gmail.com>
2021-10-25 22:21:19 -0400

    update data handling

--- Client_Website/hotel_client_side/src/utils/validationRules.ts
+++ Client_Website/hotel_client_side/src/utils/validationRules.ts
@@ -171,6 +171,7 @@ export function validateRoomTypeRule(): RegisterOptions<
   "roomType"
 > {
   return {
+    validate: (value: string) =>
       value.trim() === "Select room type"
         ? "You must choice the Type of rooms"
         : true,
