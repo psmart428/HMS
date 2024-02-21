5e09a89c504b0700408159acac729d99dd8b387b
psmart28 <powersmart28@gmail.com>
2024-02-20 21:27:34 -0400

    adjust helper behavior

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250416133232_DeleteTableReservatio.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250416133232_DeleteTableReservatio.cs
@@ -55,6 +55,7 @@ namespace HotelDataAceess.Migrations
                 {
                     table.PrimaryKey("PK__Reservat__B7EE5F04B141DD12", x => x.ReservationID);
                     table.ForeignKey(
+                        name: "FK__Reservati__Reser__5BE2A6F2",
                         column: x => x.ReservationPersonID,
                         principalTable: "People",
                         principalColumn: "PersonID",
