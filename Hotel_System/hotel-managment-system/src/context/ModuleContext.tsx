f0ddca6caed9d714b8c336ae0cc420b807a0d9c0
psmart28 <powersmart28@gmail.com>
2024-03-28 10:35:29 -0400

    revise edge-case handling

--- Hotel_System/hotel-managment-system/src/context/ModuleContext.tsx
+++ Hotel_System/hotel-managment-system/src/context/ModuleContext.tsx
@@ -15,7 +15,6 @@ interface ModuleContextType {
   openId: number;
   setOpenId: React.Dispatch<React.SetStateAction<number>>;
   modalType: ModalType;
-  setModalType: React.Dispatch<React.SetStateAction<ModalType>>;
   onCloseModuleMenu: () => void;
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
--- Hotel_System/hotel-managment-system/src/services/models/User.ts
+++ Hotel_System/hotel-managment-system/src/services/models/User.ts
@@ -15,7 +15,6 @@ export interface User {
   personDto: Person;
   email: string;
   password: string;
-  role: string;
 }
 
 export interface Login {
