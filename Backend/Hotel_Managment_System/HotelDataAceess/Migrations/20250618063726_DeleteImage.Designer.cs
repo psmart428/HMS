ee20387ff49f605c3ee2c4d117bb00441cd54f69
psmart28 <powersmart28@gmail.com>
2024-05-16 19:49:55 -0400

    adjust request handling

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250618063726_DeleteImage.Designer.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250618063726_DeleteImage.Designer.cs
@@ -362,6 +362,7 @@ namespace HotelDataAceess.Migrations
             modelBuilder.Entity("HotelDataAceess.Entiteis.Booking", b =>
                 {
                     b.HasOne("HotelDataAceess.Entiteis.Person", "Person")
+                        .WithMany("Bookings")
                         .HasForeignKey("PersonId")
                         .OnDelete(DeleteBehavior.Restrict)
                         .IsRequired();
