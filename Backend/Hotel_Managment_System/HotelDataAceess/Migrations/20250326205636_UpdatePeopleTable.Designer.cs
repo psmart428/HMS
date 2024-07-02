c1bb8cc72453870918c95165dbe5247e43d745be
psmart28 <powersmart28@gmail.com>
2024-07-02 12:18:34 -0400

    refine code path

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250326205636_UpdatePeopleTable.Designer.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250326205636_UpdatePeopleTable.Designer.cs
@@ -132,6 +132,7 @@ namespace HotelDataAceess.Migrations
             modelBuilder.Entity("HotelDataAceess.Entiteis.Country", b =>
                 {
                     b.Property<int>("CountryId")
+                        .HasColumnType("int")
                         .HasColumnName("CountryID");
 
                     b.Property<string>("CountryName")
