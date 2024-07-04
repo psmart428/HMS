3210004be4cdddeaa9055f97541b1edf8281992a
psmart28 <powersmart28@gmail.com>
2024-07-04 14:53:32 -0400

    reorganize helper logic

--- Hotel_System/hotel-managment-system/src/pages/BookingManagement.tsx
+++ Hotel_System/hotel-managment-system/src/pages/BookingManagement.tsx
@@ -42,7 +42,6 @@ export default function BookingManagement() {
               <BookingOperation />
             </div>
             <div
-              className="
                 w-full
                 sm:w-auto
                 flex justify-end
