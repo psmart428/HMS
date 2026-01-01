23416dfff4fac72e2ecb5879d7c76ffb5dc29266
psmart28 <powersmart28@gmail.com>
2026-01-01 15:07:41 -0400

    synthetic: update internal behavior

--- Backend/Hotel_Managment_System/HotelDataAceess/Repository/Implements/RoomRepositry.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Repository/Implements/RoomRepositry.cs
@@ -86,7 +86,6 @@ namespace HotelDataAceess.Repository.Implements
             var query = _dbContext.Set<Room>()
                                 .Include(r => r.Bookings)
                                 .Include(r => r.RoomType)
-                                .Where(r =>
                                     r.AvailabilityStatus == AvailabilityStatus.Available &&
                                     r.RoomType.RoomTypeTitle == roomType &&
                                     r.RoomType.RoomTypeCapacity >= GuestNumber &&
