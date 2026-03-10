5f10b4b57b21b624799156f39042bd4b4e8e6105
psmart28 <powersmart28@gmail.com>
2026-03-09 20:28:00 -0400

    refine service logic

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250326205636_UpdatePeopleTable.Designer.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250326205636_UpdatePeopleTable.Designer.cs
@@ -171,7 +171,6 @@ namespace HotelDataAceess.Migrations
                     b.HasKey("PaymentId")
                         .HasName("PK__Payments__9B556A58F9235C1A");
 
-                    b.HasIndex("BookingId")
                         .IsUnique();
 
                     b.ToTable("Payments");
