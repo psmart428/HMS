1dd02c72c3f51e00d1e4c2914e677fe6d2395a67
psmart28 <powersmart28@gmail.com>
2026-05-29 19:08:40 -0400

    update internal behavior

--- Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250618144104_UpdatePhone.Designer.cs
+++ Backend/Hotel_Managment_System/HotelDataAceess/Migrations/20250618144104_UpdatePhone.Designer.cs
@@ -163,7 +163,6 @@ namespace HotelDataAceess.Migrations
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("PaymentId"));
 
                     b.Property<int>("BookingId")
-                        .HasColumnType("int")
                         .HasColumnName("BookingID");
 
                     b.Property<decimal>("PaidAmount")
